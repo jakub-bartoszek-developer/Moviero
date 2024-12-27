dedd45791618c8ef4e267b09d638dd26a1275112
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-12-27 22:52:59 +0100

    Split popular movies to more components

--- src/pages/Movies/PopularMovies/PopularMovies.jsx
+++ src/pages/Movies/PopularMovies/PopularMovies.jsx
@@ -1,72 +1,34 @@
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
-import { nanoid } from "nanoid";
-import {
- Banner,
- Description,
- Genre,
- Genres,
- MovieDetails,
- OutOf,
- Poster,
- PosterWrapper,
- Rate,
- Rating,
- StarIcon,
- Votes,
- PopularMoviesList,
- Title,
- SimilarMoviesList,
- Container
-} from "./styled";
+import { PopularMoviesList, Container } from "./styled";
 import { Loader } from "../../../components/Loader/Loader";
 import { Pagination } from "../../../components/Pagination/Pagination";
-import { toMovie } from "../../../routes";
 import { VerticalSection } from "../../../components/VerticalSection/styled";
-import { HorizontalSection } from "../../../components/HorizontalSection/styled";
 import { SectionHeader } from "../../../components/SectionHeader/styled";
 import {
  fetchPopularMovies,
- fetchSimilarMovies,
  selectGenres,
  selectPopularMovies,
  selectRandomPopularMovie,
- selectSimilarMovies,
  selectStatus,
  selectTotalPages,
  setRandomPopularMovie
 } from "../../../utils/redux/moviesSlice";
 import { MovieTile } from "../../../components/MovieTile/MovieTile";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
+
+import { Header } from "../../../components/Header/Header";
 
 export const PopularMovies = () => {
  const dispatch = useDispatch();
  const status = useSelector(selectStatus);
  const popularMovies = useSelector(selectPopularMovies);
- const similarMovies = useSelector(selectSimilarMovies);
+ const randomPopularMovie = useSelector(selectRandomPopularMovie);
  const totalPages = useSelector(selectTotalPages);
  const genres = useSelector(selectGenres);
- const [currentMovie, setCurrentMovie] = useState(popularMovies[0]);
- const randomPopularMovie = useSelector(selectRandomPopularMovie);
- const movieGenres = genres?.filter((genre) =>
-  currentMovie?.genre_ids.includes(genre.id)
- );
- const [cancel, setCancel] = useState(false);
+ const [currentMovie, setCurrentMovie] = useState(randomPopularMovie);
  const [searchParams, setSearchParams] = useSearchParams();
  const containerRef = useRef(null);
-
- const handleClickScroll = (e) => {
-  const element = e.target;
-  if (element) {
-   element.scrollIntoView({
-    behavior: "smooth",
-    inline: "center",
-    block: "end"
-   });
-  }
- };
 
  useEffect(() => {
   if (searchParams.get("page")) {
@@ -85,82 +47,26 @@ export const PopularMovies = () => {
  useEffect(() => {
   if (Object.keys(randomPopularMovie).length > 0) {
    setCurrentMovie(randomPopularMovie);
-   setCancel(false);
   }
- }, [dispatch, randomPopularMovie]);
-
- useEffect(() => {
-  if (!cancel && currentMovie && Object.keys(currentMovie).length > 0) {
-   dispatch(
-    fetchSimilarMovies({ genreIds: currentMovie.genre_ids.join("%2C") })
-   );
-   setCancel(true);
-  }
- }, [dispatch, cancel, currentMovie]);
+ }, [randomPopularMovie]);
 
  switch (status) {
-  case `success`:
+  case "success":
    return (
     <Container ref={containerRef}>
-     {currentMovie && (
-      <Banner
-       key={currentMovie.id}
-       $bgImage={`https://image.tmdb.org/t/p/original/${currentMovie.backdrop_path}`}
-      >
-       <MovieDetails>
-        <Title to={toMovie({ id: currentMovie.id })}>
-         {currentMovie.title}
-        </Title>
-        <Genres>
-         {movieGenres.map((genre) => (
-          <Genre key={nanoid()}>{genre.name}</Genre>
-         ))}
-        </Genres>
-        <Rating>
-         <StarIcon />
-         <Rate>
-          {currentMovie?.vote_average.toFixed(1)}/<OutOf>10</OutOf>
-          <Votes>&nbsp;{currentMovie?.vote_count} votes</Votes>
-         </Rate>
-        </Rating>
-        <Description>{currentMovie?.overview}</Description>
-       </MovieDetails>
-      </Banner>
-     )}
-     {similarMovies.length && (
-      <HorizontalSection>
-       <SectionHeader>Similar movies</SectionHeader>
-       <SimilarMoviesList>
-        {similarMovies.map((movie) => (
-         <PosterWrapper
-          $current={movie.id === currentMovie.id}
-          key={nanoid()}
-          onClick={(e) => {
-           handleClickScroll(e);
-           setCurrentMovie(movie);
-          }}
-         >
-          <Poster>
-           <LazyLoadImage
-            alt={movie.title}
-            src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
-            effect="blur"
-            height="100%"
-           />
-          </Poster>
-         </PosterWrapper>
-        ))}
-       </SimilarMoviesList>
-      </HorizontalSection>
-     )}
-     {popularMovies.length && (
+     <Header
+      currentMovie={currentMovie}
+      setCurrentMovie={setCurrentMovie}
+      genres={genres}
+     />
+     {popularMovies.length > 0 && (
       <VerticalSection>
        <SectionHeader>Popular movies</SectionHeader>
        <PopularMoviesList>
         {popularMovies.map((movie) => (
          <MovieTile
           genres={genres}
-          key={nanoid()}
+          key={movie.id}
           movie={movie}
          />
         ))}
