f665d1abd959cd81d6c39ad53d498f4b5527ee29
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-12-27 22:53:11 +0100

    Created banner component

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,41 @@
+import { nanoid } from "nanoid";
+import { toMovie } from "../../routes";
+import {
+ MovieDetails,
+ BannerWrapper,
+ Title,
+ Genres,
+ Genre,
+ Rating,
+ StarIcon,
+ Rate,
+ Description,
+ Votes,
+ OutOf
+} from "./styled";
+
+export const Banner = ({ currentMovie, movieGenres }) => {
+ return (
+  <BannerWrapper
+   key={currentMovie.id}
+   $bgImage={`https://image.tmdb.org/t/p/original/${currentMovie.backdrop_path}`}
+  >
+   <MovieDetails>
+    <Title to={toMovie({ id: currentMovie.id })}>{currentMovie.title}</Title>
+    <Genres>
+     {movieGenres.map((genre) => (
+      <Genre key={nanoid()}>{genre.name}</Genre>
+     ))}
+    </Genres>
+    <Rating>
+     <StarIcon />
+     <Rate>
+      {(currentMovie?.vote_average ?? 0).toFixed(1)}/<OutOf>10</OutOf>
+      <Votes>&nbsp;{currentMovie?.vote_count} votes</Votes>
+     </Rate>
+    </Rating>
+    <Description>{currentMovie?.overview}</Description>
+   </MovieDetails>
+  </BannerWrapper>
+ );
+};
---
+++ src/components/Banner/styled.jsx
@@ -0,0 +1,157 @@
+import styled from "styled-components";
+import { ReactComponent as Star } from "../../assets/icons/star.svg";
+import { Link } from "react-router-dom";
+
+export const BannerWrapper = styled.div`
+ box-shadow: inset 0px 0px 80px 100px
+   ${({ theme }) => theme.colors.site.background},
+  inset 0px 0px 50px 50px ${({ theme }) => theme.colors.site.background};
+ width: 100%;
+ height: 100vw;
+ max-height: 570px;
+ background-image: ${({ $bgImage }) => `url(${$bgImage})`};
+ background-size: cover;
+ background-position: top;
+ border: 1px solid ${({ theme }) => theme.colors.site.background};
+ animation: loading 1s ease-in;
+
+ @keyframes loading {
+  0% {
+   opacity: 0%;
+  }
+  100% {
+   opacity: 100%;
+  }
+ }
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
+  box-shadow: inset 0px 0px 60px 80px
+   ${({ theme }) => theme.colors.site.background};
+ }
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  gap: 8px;
+  box-shadow: inset 0px 0px 30px 50px
+   ${({ theme }) => theme.colors.site.background};
+ }
+`;
+
+export const MovieDetails = styled.div`
+ padding-top: 82px;
+ width: 100%;
+ display: flex;
+ flex-direction: column;
+ gap: 16px;
+ height: 100%;
+`;
+
+export const Title = styled(Link)`
+ text-decoration: none;
+ color: ${({ theme }) => theme.colors.site.text};
+ font-weight: 500;
+ font-size: 72px;
+ padding: 0;
+ margin: 0;
+ text-shadow: 0 0 10px black, 0 0 20px black;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.l}) {
+  font-size: 64px;
+ }
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
+  font-size: 52px;
+ }
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  font-size: 40px;
+ }
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
+  font-size: 24px;
+ }
+`;
+
+export const Genres = styled.div`
+ display: flex;
+ flex-wrap: wrap;
+ gap: 8px;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  gap: 4px;
+ }
+`;
+
+export const Genre = styled.div`
+ background-color: ${({ theme }) => theme.colors.site.blurredBackground};
+ padding: 6px;
+ font-size: 12px;
+ border-radius: 5px;
+ text-shadow: 0 0 5px black, 0 0 10px black, 0 0 20px black, 0 0 30px black;
+ backdrop-filter: blur(10px);
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  padding: 4px;
+  font-size: 10px;
+ }
+`;
+
+export const Rating = styled.div`
+ display: flex;
+ gap: 8px;
+ align-items: center;
+ text-shadow: 0 0 5px black, 0 0 10px black, 0 0 20px black, 0 0 30px black;
+`;
+
+export const StarIcon = styled(Star)`
+ color: yellow;
+ height: 24px;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  height: 16px;
+ }
+`;
+
+export const Rate = styled.span`
+ font-size: 24px;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  font-size: 16px;
+ }
+`;
+
+export const OutOf = styled.span`
+ font-size: 16px;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  font-size: 10px;
+ }
+`;
+
+export const Votes = styled.span`
+ font-weight: 300;
+ color: ${({ theme }) => theme.colors.site.mutedText};
+ font-size: 16px;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  font-size: 10px;
+ }
+`;
+
+export const Description = styled.p`
+ padding: 6px;
+ width: 50%;
+ overflow-y: auto;
+ border-radius: 10px;
+ background-color: ${({ theme }) => theme.colors.site.blurredBackground};
+ font-size: 16px;
+ font-weight: 300;
+ backdrop-filter: blur(5px);
+ text-shadow: 0 0 5px black, 0 0 5px black, 0 0 5px black, 0 0 5px black;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  font-size: 12px;
+  width: 100%;
+  margin-top: auto;
+  margin-bottom: 0;
+ }
+`;
