dce514b13c02d0055f958867b5b45c71db9daeb0
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-12-30 11:16:07 +0100

    Merged people in cast and crew

--- src/utils/redux/personSlice.jsx
+++ src/utils/redux/personSlice.jsx
@@ -5,6 +5,7 @@ const personSlice = createSlice({
  initialState: {
   person: {},
   personCredits: {},
+  personMovies: [],
   status: "loading"
  },
  reducers: {
@@ -15,17 +16,26 @@ const personSlice = createSlice({
   setPersonCredits: (state, { payload }) => {
    state.personCredits = payload;
   },
+  setPersonMovies: (state, { payload }) => {
+   state.personMovies = payload;
+  },
   setStatus: (state, { payload }) => {
    state.status = payload;
   }
  }
 });
 
-export const { fetchPerson, setPerson, setStatus, setPersonCredits } =
- personSlice.actions;
+export const {
+ fetchPerson,
+ setPerson,
+ setStatus,
+ setPersonCredits,
+ setPersonMovies
+} = personSlice.actions;
 
 export const selectPerson = (state) => state.person.person;
 export const selectPersonCredits = (state) => state.person.personCredits;
+export const selectPersonMovies = (state) => state.person.personMovies;
 export const selectStatus = (state) => state.person.status;
 
 export default personSlice.reducer;
--- src/utils/saga/personSaga.js
+++ src/utils/saga/personSaga.js
@@ -3,23 +3,30 @@ import {
  fetchPerson,
  setPerson,
  setPersonCredits,
- setStatus
+ setStatus,
+ setPersonMovies
 } from "../redux/personSlice";
 import { fetchAPI } from "../fetchAPI";
 
 function* fetchMovieHandler({ payload }) {
  try {
   yield put(setStatus("loading"));
+
   const person = yield call(
    fetchAPI,
    `person/${payload.personId}?language=en-US`
   );
+
   const personCredits = yield call(
    fetchAPI,
    `person/${payload.personId}/credits?language=en-US`
   );
+
+  const personMovies = [...personCredits.cast, ...personCredits.crew];
+
   yield put(setPerson(person));
   yield put(setPersonCredits(personCredits));
+  yield put(setPersonMovies(personMovies));
   yield put(setStatus("success"));
  } catch (error) {
   yield put(setStatus("error"));
