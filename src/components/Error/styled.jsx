24d97718e8d105b84cb88744af50d2e9cc091eb5
Jakub Bartoszek <k.bartoszek507@wp.pl>
2025-01-02 15:21:23 +0100

    Renamed Container to ErrorWrapper

--- src/components/Error/styled.jsx
+++ src/components/Error/styled.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { ReactComponent as Error } from "../../assets/icons/error.svg";
 
-export const Container = styled.div`
+export const ErrorWrapper = styled.div`
  height: 100%;
  width: 100%;
  display: flex;
