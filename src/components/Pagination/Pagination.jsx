7fd2c16f26f223edf910cf40681cef0d1d9aea4f
Jakub Bartoszek <k.bartoszek507@wp.pl>
2025-01-02 15:08:43 +0100

    Removed containerRef from Pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,19 +1,10 @@
 import { Container, PageCount, StyledButton } from "./styled";
 
-export const Pagination = ({
- searchParams,
- setSearchParams,
- containerRef,
- totalPages
-}) => {
+export const Pagination = ({ searchParams, setSearchParams, totalPages }) => {
  const page = parseInt(searchParams.get("page"));
 
  const handlePageClickScroll = () => {
-  if (containerRef.current) {
-   containerRef.current.scrollIntoView({
-    alignToTop: true
-   });
-  }
+  window.scrollTo(0, 0);
  };
 
  const onFirstPageHandler = () => {
